7ac8ed3e22702c38ef64136a52d5ee719f6b551d
FazioNico <contact@nicolasfazio.ch>
2026-05-18 12:26:24 +0200

    feat: restructure app with Ionic components and routing
    - Updated app.html to use <ion-app> and <ion-router-outlet> for Ionic integration.
    - Defined routes for Home and Settings components in app.routes.ts.
    - Created HomeComponent with a button to navigate to Settings and a chip element.
    - Created SettingsComponent with a simple header.
    - Added styles for HomeComponent and SettingsComponent.
    - Implemented unit tests for both HomeComponent and SettingsComponent.
    - Introduced a new variables.css file for dark theme support in the app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IonRouterOutlet, IonApp, IonFooter } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pages/settings/settings.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss'],
+  imports: [IonContent]
+})
+export class SettingsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
